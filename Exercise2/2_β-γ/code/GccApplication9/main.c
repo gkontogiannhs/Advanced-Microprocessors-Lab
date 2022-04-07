47d8018714891e8c979929cc977233812b0146e1
George Kontogiannis <100489926+gkontogiannhs@users.noreply.github.com>
2022-04-07 18:23:25 +0300

    Updated reverse code and interrupts

--- Exercise2/2_β-γ/code/GccApplication9/main.c
+++ Exercise2/2_β-γ/code/GccApplication9/main.c
@@ -2,8 +2,13 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+#define t 10; // 1 second delay
+
 int cnt = 0;
 int still = 1;
+int t_interr = 0;
+int result = 0;
+int reverse = 0;
 
 int main(void)
 {
@@ -32,21 +37,34 @@ int main(void)
 	
 	// limit is 5
 	ADC0.WINLT |= 5;
+	ADC0.WINHT |= 20;
 	ADC0.INTCTRL |= ADC_WCMP_bm;
 	ADC0.CTRLE |= ADC_WINCM0_bm;
 	
-	// int
+	// switch interrupt enable
 	PORTF.PIN5CTRL |= (PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc);
 	
-	sei();
-	// start conversion
-	ADC0.COMMAND |= ADC_STCONV_bm;
+	//Timer set up
+	TCA0.SINGLE.CNT = 0; //Clear counter
+	TCA0.SINGLE.CTRLB = 0; //Normal Mode
+	TCA0.SINGLE.CMP0 = t; //Stop turning when this value is reached
+	TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1024_gc;
+	TCA0.SINGLE.INTCTRL = TCA_SINGLE_CMP0_bm;
 	
 	while(1){
 		// go straight
+		sei();
+		// start conversion
+		ADC0.COMMAND |= ADC_STCONV_bm;
+		
 		PORTD.OUTCLR = PIN2_bm;
+		_delay_ms(1);
+		// check for right distance
+		single();
+		
+		// perimeter is completed. Stop device
 		if(!still)
-			break;
+		break;
 	}
 }
 
@@ -54,22 +72,54 @@ int main(void)
 ISR(ADC0_WCOMP_vect){
 	int intflags = ADC0.INTFLAGS;
 	ADC0.INTFLAGS = intflags;
-	// increase counter
-	cnt++;
-	// if 1, 4, 5, 6, 7, turn left
-	if(cnt == 1 || cnt == 4 || cnt == 5 || cnt == 6 || cnt == 7){
-		turnLeft();
-	}
-	// if 2, 3 turn right
-	else if(cnt == 2 || cnt == 3){
-		turnRight();
-	}
 	
+	if(!reverse){
+		if (result <=5){
+			cnt++;
+			turnLeft();
+
+			}else if(result >= 20){
+			cnt++;
+			turnRight();
+		}
+		// reverse mode enabled
+		}else{
+		// increase counter
+		if (result <=5){
+			cnt--;
+			turnRight();
+
+			}else if(result >= 20){
+			cnt--;
+			turnLeft();
+		}
+	}
+	// CLEAR RES
+	ADC0.RES = 0;
 	// if perimeter completed
-	if(cnt >= 7)
+	if(cnt >= 7 || cnt <= 0)
 	still = 0;
 	
 }
+
+ISR(TCA0_CMP0_vect){
+	//TCA0.SINGLE.CTRLA = 0; //Disable Timer
+	TCA0.SINGLE.CTRLA &= 0xFE;
+	//Clear timer interrupt flag
+	int intflags = TCA0.SINGLE.INTFLAGS;
+	TCA0.SINGLE.INTFLAGS = intflags;
+	t_interr = 1;
+}
+
+
+ISR(PORTF_PORT_vect) {
+	int intflags = PORTF.INTFLAGS;
+	PORTF.INTFLAGS = intflags;
+	
+	reverse = 1;
+}
+
+
 
 
 void turnLeft(){
@@ -79,6 +129,16 @@ void turnLeft(){
 	// turn on left led
 	PORTD.OUTCLR = PIN0_bm;
 	_delay_ms(2);
+	// enable timer
+	/*
+	TCA0.SINGLE.CTRLA |= 1;
+	sei();
+	while(!t_interr){
+		// wait timer
+	}
+	//TCA0.SINGLE.CTRLA &= 0xFE;
+	cli();
+	*/
 	// turn off left led
 	PORTD.OUT |= PIN0_bm;
 	// turn straight led on
@@ -99,83 +159,24 @@ void turnRight(){
 }
 
 
-ISR(PORTF_PORT_vect) {
-	int intflags = PORTF.INTFLAGS;
-	PORTF.INTFLAGS = intflags;
+void single(){
+	// clear ADC
+	ADC0.CTRLA = 0;
+	// initialize ADC
+	ADC0.CTRLA |= ADC_RESSEL_10BIT_gc;
+	ADC0.CTRLA |= ADC_ENABLE_bm;
 	
-	if(cnt == 1){
-		// turn straight led on
-		PORTD.OUTCLR = PIN2_bm;
-		_delay_ms(1);
-		turnRight();
-		_delay_ms(2);
-	}
-	else if(cnt == 2){
-		// turn straight led on
-		PORTD.OUTCLR = PIN2_bm;
-		_delay_ms(1);
-		turnLeft();
-		_delay_ms(1);
-		turnRight();
-		_delay_ms(2);
-	}
-	else if(cnt == 3){
-		// turn straight led on
-		PORTD.OUTCLR = PIN2_bm;
-		_delay_ms(1);
-		turnLeft();
-		_delay_ms(1);
-		turnLeft();
-		_delay_ms(1);
-		turnRight();
-		_delay_ms(2);
-	}
-	else if(cnt == 4){
-		// turn straight led on
-		PORTD.OUTCLR = PIN2_bm;
-		_delay_ms(1);
-		turnRight();
-		_delay_ms(1);
-		turnLeft();
-		_delay_ms(1);
-		turnLeft();
-		_delay_ms(1);
-		turnRight();
-		_delay_ms(2);
-	}
-	else if(cnt == 5){
-		// turn straight led on
-		PORTD.OUTCLR = PIN2_bm;
-		_delay_ms(4);
-		turnRight();
-		_delay_ms(1);
-		turnRight();
-		_delay_ms(1);
-		turnLeft();
-		_delay_ms(1);
-		turnLeft();
-		_delay_ms(1);
-		turnRight();
-		_delay_ms(2);
-	}
-	else if(cnt == 6){
-		// turn straight led on
-		PORTD.OUTCLR = PIN2_bm;
-		_delay_ms(5);
-		turnRight();
-		_delay_ms(4);
-		turnRight();
-		_delay_ms(1);
-		turnRight();
-		_delay_ms(1);
-		turnLeft();
-		_delay_ms(1);
-		turnLeft();
-		_delay_ms(1);
-		turnRight();
-		_delay_ms(2);
-	}
+	ADC0.CTRLE = 0x02;
+	sei();
+	// start conversion
+	ADC0.COMMAND |= ADC_STCONV_bm;
+	result = ADC0.RES;
+	_delay_ms(2);
 	
+	// re-set ADC to free-running
+	ADC0.CTRLA |= ADC_FREERUN_bm;
+	ADC0.CTRLE = 0x01;
+	ADC0.DBGCTRL |= ADC_DBGRUN_bm;
+	// start conversion
+	ADC0.COMMAND |= ADC_STCONV_bm;
 }
-
-
